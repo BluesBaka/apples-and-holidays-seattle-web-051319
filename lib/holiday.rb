9fc31997c73f5c8769562ee81a287b8f3836e9a5
Bryna Dunnells <badunnells@gmail.com>
2019-05-08 00:10:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday.collect do |holiday, supplies|
         if supplies.include?("BBQ")
             holiday_bbq << holiday
-            binding.pry
+            
       end
       
     end
